2a1b805d1d6b563be69ac48130c12a298962cf0a
pogoyoly <pogoyoly@gmail.com>
2024-10-13 13:13:14 +0200

    bug with indexing fixed

--- R/establish_by_pac.R
+++ R/establish_by_pac.R
@@ -327,7 +327,7 @@ establish_by_place_conquer<-function(potential_space,
 
 
   for(i in 2:length(patched_raster)){
-    mati <- terra::as.matrix(patched_raster[[i]])
+    mati <- terra::as.matrix(terra::ext(patched_raster[[i]]))
     dimnames(mati) <- list(x = 1:nrow(mati), y = 1:ncol(mati))
     #mydf <- reshape2::melt(mati,level = 1, varnames = c("x", "y"))  # Ensure proper names
     #print(mydf)
