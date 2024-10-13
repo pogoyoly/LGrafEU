0b2d93007395751e62ddcf5be1eb2029373fe7e6
pogoyoly <pogoyoly@gmail.com>
2024-10-13 11:10:47 +0200

    for debugging on the cmd

--- R/establish_by_pac.R
+++ R/establish_by_pac.R
@@ -329,6 +329,7 @@ establish_by_place_conquer<-function(potential_space,
   for(i in 2:length(patched_raster)){
     mati <- raster::as.matrix(patched_raster[[i]])
     dimnames(mati) <- list(x = 1:nrow(mati), y = 1:ncol(mati))
+    print(dim(mati))
     mydf <- reshape2::melt(mati,level = 1, varnames = c("x", "y"))  # Ensure proper names
     names(mydf) <- c("x", "y", "Z")
     newdata <- mydf[!is.na(mydf$Z),]  # Filter out NAs
