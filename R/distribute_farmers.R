83bd510d18c015c0581772557da857b12bb80b4b
pogoyoly <pogoyoly@gmail.com>
2024-11-19 18:19:27 +0100

    structured farmer distribution not working yet, but almost there

--- R/distribute_farmers.R
+++ R/distribute_farmers.R
@@ -75,11 +75,76 @@ distrubution_by_farmer <- function(output_obj, assign_mode = 1, mean_fields_per_
     }
 
     #mode 2 structured distribution
-    if(assign_mode == 2){
+  if (assign_mode == 2) {
+
+    # Extract the number of fields from the output object
+    num_fields <- length(obj$field_list)
+
+    # Create an empty list to store the last assigned field for each farmer
+    farmer_fields <- list()
+
+    k <- 1
+    farmer_num <- 1
+
+    # Run loop until all fields have been distributed
+    while (k <= num_fields) {
+
+      # Set the number of fields to assign to a farmer based on the distribution
+      if (distribution == "norm") {
+        ran_fields <- rnorm(1, mean = mean_fields_per_farm, sd = sd_fields_per_farm)
+        ran_fields <- ceiling(ran_fields)
+      }
+      if (distribution == "lnorm") {
+        mu_N <- log(mean_fields_per_farm^2 / sqrt(sd_fields_per_farm^2 + mean_fields_per_farm^2))
+        sigma_N <- sqrt(log(1 + (sd_fields_per_farm^2 / mean_fields_per_farm^2)))
+        ran_fields <- max(1, round(rlnorm(1, meanlog = mu_N, sdlog = sigma_N)))
+      }
 
 
+      for (q in 1:ran_fields) {
+        if (k > num_fields) {
+          break
+        }
+
+        # Assign the first field randomly if the farmer has no fields yet
+        if (length(farmer_fields[farmer_num]) == 1) {
+          obj$field_list[[k]]@farmer <- farmer_num
+          farmer_fields[farmer_num] <- obj$field_list[[k]]  # Store the first assigned field
+        } else {
+          # Find the centroid of the last assigned field
+          last_field <- farmer_fields[farmer_num]
+          last_centroid <- c(
+            mean(last_field$location[1]),  # Mean of x-coordinates
+            mean(last_field$location[2])   # Mean of y-coordinates
+          )
+
+          print(last_centroid)
+          # Calculate distances to the centroids of remaining fields
+          remaining_fields <- obj$field_list[k:num_fields]
+          distances <- sapply(remaining_fields, function(field) {
+            current_centroid <- c(
+              mean(field@location[[1]]),  # Mean of x-coordinates
+              mean(field@location[[2]])   # Mean of y-coordinates
+            )
+            sqrt((current_centroid[1] - last_centroid[1])^2 +
+                   (current_centroid[2] - last_centroid[2])^2)
+          })
+
+          # Find the nearest field
+          nearest_index <- which.min(distances) + k - 1
+          obj$field_list[nearest_index]@farmer <- farmer_num
+          farmer_fields[farmer_num] <- obj$field_list[nearest_index]  # Update last assigned field
+
+          # Swap the nearest field with the current field to maintain order
+          obj$field_list[c(k, nearest_index)] <- obj$field_list[c(nearest_index, k)]
+        }
+
+        k <- k + 1
+      }
+
+      farmer_num <- farmer_num + 1
     }
-
+  }
 
 
 
